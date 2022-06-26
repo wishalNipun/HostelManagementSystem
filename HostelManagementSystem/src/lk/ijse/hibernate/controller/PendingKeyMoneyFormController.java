b466e4152d601038343ee968a822b4aa8b91aaba
wishalNipun <wishal1974@gmail.com>
2022-06-26 13:56:34 +0530

    add selection textFields and refactored disable enable funtionlities

--- HostelManagementSystem/src/lk/ijse/hibernate/controller/PendingKeyMoneyFormController.java
+++ HostelManagementSystem/src/lk/ijse/hibernate/controller/PendingKeyMoneyFormController.java
@@ -14,7 +14,7 @@ import lk.ijse.hibernate.dto.CustomDTO;
 import java.util.List;
 
 public class PendingKeyMoneyFormController {
-    public TableView <Object>tblReservation;
+    public TableView <CustomDTO>tblReservation;
     public JFXTextField txtStudentId;
     public JFXComboBox cmbPaymentStatus;
     public JFXButton btnUpdate;
@@ -32,6 +32,7 @@ public class PendingKeyMoneyFormController {
     public TableColumn colKeyMoney;
     public JFXTextField txtType;
     public JFXTextField txtRoomTypeId;
+    public JFXTextField txtKeyMoney;
 
     public void initialize(){
         colReservationId.setCellValueFactory(new PropertyValueFactory<>("res_id"));
@@ -43,6 +44,40 @@ public class PendingKeyMoneyFormController {
         colPaymentStatus.setCellValueFactory(new PropertyValueFactory<>("status"));
         colDate.setCellValueFactory(new PropertyValueFactory<>("date"));
         loadAllData();
+
+        txtReservationId.setEditable(false);
+        txtName.setEditable(false);
+        txtRoomTypeId.setEditable(false);
+        txtStudentId.setEditable(false);
+        txtType.setEditable(false);
+        txtKeyMoney.setEditable(false);
+        txtDate.setEditable(false);
+        btnUpdate.setDisable(true);
+
+        tblReservation.getSelectionModel().selectedItemProperty().addListener((observable, oldValue, newValue) -> {
+            if (newValue!= null){
+                btnUpdate.setDisable(false);
+                txtReservationId.setText(newValue.getRes_id());
+                txtName.setText(newValue.getName());
+                txtRoomTypeId.setText(newValue.getRoom_type_id());
+                txtStudentId.setText(newValue.getStudent_id());
+                txtType.setText(newValue.getType());
+                txtKeyMoney.setText(String.valueOf(newValue.getKey_money()));
+                cmbPaymentStatus.setValue(newValue.getStatus());
+                txtDate.setText(String.valueOf(newValue.getDate()));
+            }else {
+                txtReservationId.clear();
+                txtName.clear();
+                txtRoomTypeId.clear();
+                txtStudentId.clear();
+                txtType.clear();
+                txtKeyMoney.clear();
+                cmbPaymentStatus.getSelectionModel().clearSelection();
+                txtDate.clear();
+            }
+        });
+
+        cmbPaymentStatus.getItems().addAll("Paid","Paid Later");
     }
 
     private void loadAllData() {
